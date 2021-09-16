1fa188ea7acb70cf1d4fe5bdd4d1877eb2519df7
= <=>
2021-09-16 16:20:07 +0800

    init

---
+++ src/main/java/cn/vincent/controller/MainController.java
@@ -0,0 +1,15 @@
+package cn.vincent.controller;
+
+import cn.vincent.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @ResponseBody
+    @GetMapping("/getJson")
+    public User getJson(){
+        return new User("ABC", 22);
+    }
+}
---
+++ src/test/java/cn/vincent/Boot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vincent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
